c2421902b77f98cc0bd2936bba3ac4e2f689d43f
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-08 09:02:59 +0900

    Update : Sound Manager
    1. Extensibility Update: Grouped the classification values, previously limited to BGM and SFX, into an enum for easy addition and removal.
    2. Extensive Error Codes Added: Added error message functionality for invalid settings, such as selecting the Master Type.
    3. Pause/Unpause Functionality Added.
    4. More Granular Control for Mute/Volume Adjustments.
    5. State Change Code Added.

--- Library/Assets/Scripts/03.Manager/Sound.cs
+++ Library/Assets/Scripts/03.Manager/Sound.cs
@@ -5,13 +5,41 @@ namespace Manager
 {
     public class Sound : Util.Inherited.DisposableSingleton<Sound>
     {
+        public enum Type
+        {
+            BGM,
+            EFFECT,
+            VOICE,
+            MASTER
+        }
         [System.Serializable]
         private class Clip
         {
             public string id;
-            public float defaultVolum = 0.5f;
+            public Type type;
             public AudioClip audio;
             public int priority;
+            public float defaultVolum = 0.5f;
+        }
+
+        [System.Serializable]
+        public class State
+        {
+            public bool mute;
+            public float volum;
+            [ReadOnly] public string baseKey;
+
+            public State(bool mute, float volum, string baseKey)
+            {
+                this.mute = mute;
+                this.volum = volum;
+                this.baseKey = baseKey;
+            }
+
+            public State Clone()
+            {
+                return new State(mute, volum, baseKey);
+            }
         }
 
         public enum Production
@@ -27,12 +55,13 @@ namespace Manager
             [Header("Fade animation data")]
             public bool onProductionUpdate;
             public Production production;
-            public float currentTime;
-            public float originalVolum;
+            [ReadOnly] public float currentTime;
+            [ReadOnly] public float originalVolum;
 
             [Header("Sound")]
-            public AudioSource player;
             public int priority;
+            [ReadOnly] public AudioSource player;
+            [ReadOnly] public string clipID;
 
             public Audio(AudioSource player)
             {
@@ -68,35 +97,24 @@ namespace Manager
         [SerializeField] Audio bgm;
         [SerializeField] List<Audio> sfx;
 
-        [Header("Show Setting")]
-        [SerializeField, ReadOnly] bool isBgmOn;
-        [SerializeField, ReadOnly] bool isSfxOn;
+        [Header("Setting")]
+        [SerializeField] bool fullStackIsSkip;
+        [SerializeField, ReadOnly] bool isPause;
         [SerializeField, ReadOnly] bool onReady;
-        [SerializeField, ReadOnly] float bgmVolum = 1;
-        [SerializeField, ReadOnly] float sfxVolum = 1;
+        Dictionary<Type, State> states;
 
         [Header("Has Data")]
         [SerializeField] float productDuration = 0.2f;
 #if UNITY_EDITOR
-        [SerializeField] int editSfxSoundCount;
+        [SerializeField] int editSfxCount;
 #endif
-        [SerializeField] List<Util.Inspector.UniPair<float, Clip>> fade;
         [SerializeField] Queue<Registration> callStack = new Queue<Registration>();
-
-        #region Property list
-        public bool IsBgmOn { get => isBgmOn; set => isBgmOn = value; }
-        public bool IsSfxOn { get => isSfxOn; set => isSfxOn = value; }
-        #endregion
-
-        //== PlayerPrefabs save key
-        private const string BgmMuteKey = "BMK_SD";
-        private const string SfxMuteKey = "SMK_SD";
-        private const string BgmVolumKey = "BVK_SD";
-        private const string SfxVolumKey = "SVK_SD";
 
         //== Variable declaration for improved readability
         private const int On = 1;
         private const int Off = 0;
+        private const string Mute = "MUTE";
+        private const string Volum = "VOLUM";
 
 #if UNITY_EDITOR
         private void OnValidate()
@@ -110,11 +128,11 @@ namespace Manager
             }
             if (sfx == null || sfx.Count == 0)
             {
-                if (editSfxSoundCount <= 0) editSfxSoundCount = 5;
+                if (editSfxCount <= 0) editSfxCount = 5;
 
                 if (sfx == null) sfx = new List<Audio>();
 
-                for (int i = 0; i < editSfxSoundCount; i++)
+                for (int i = 0; i < editSfxCount; i++)
                 {
                     GameObject sfxPlayer = new GameObject("SFX Player");
                     sfxPlayer.transform.SetParent(this.transform);
@@ -122,6 +140,19 @@ namespace Manager
                     Audio audio = new Audio(sfxPlayer.AddComponent<AudioSource>());
                     audio.player.playOnAwake = false;
                     sfx.Add(audio);
+                }
+            }
+
+            if (clips != null)
+            {
+                foreach (var item in clips)
+                {
+                    var clip = item.value;
+                    if (clip.type == Type.MASTER)
+                    {
+                        Debug.LogError($"Sound Clip cannot be of MASTER type.\n" +
+                            $"Clip id is [ {clip.id} ]");
+                    }
                 }
             }
         }
@@ -157,45 +188,120 @@ namespace Manager
 
             clips.Clear();
         }
+        private void InitializeState()
+        {
+            if (states != null && states.Count != 0) return;
+
+            states = new Dictionary<Type, State>();
+
+            var types = System.Enum.GetValues(typeof(Type));
+            foreach (var value in types)
+            {
+                Type type = (Type)value;
+
+                //== PlayerPrefabs save key
+                string baseKey = $"SET_SOUND_KEY_{type}_";
+                State state = new State(false, 1.0f, baseKey);
+                states.Add(type, state);
+            }
+        }
 
         #region Load & Save : Setting
         private void LoadSetting()
         {
-            int bgmon = PlayerPrefs.GetInt(BgmMuteKey, On);
-            if (bgmon == On) isBgmOn = true;
-            else isBgmOn = false;
+            foreach (var item in states.Values)
+            {
+                State state = item;
+                int readMute = PlayerPrefs.GetInt(state.baseKey + Mute, Off);
+                bool mute = (readMute == On) ? true : false;
+                float volum = PlayerPrefs.GetFloat(state.baseKey + Volum, 1.0f);
 
-            int sfxon = PlayerPrefs.GetInt(SfxMuteKey, On);
-            if (sfxon == On) isSfxOn = true;
-            else isSfxOn = false;
+                state.mute = mute;
+                state.volum = volum;
+            }
 
-            bgmVolum = PlayerPrefs.GetFloat(BgmVolumKey, 1);
-            sfxVolum = PlayerPrefs.GetFloat(SfxVolumKey, 1);
+            State master = states[Type.MASTER];
+            AudioListener.volume = master.volum;
         }
         private void SaveSetting()
         {
-            int bgmon = (isBgmOn) ? On : Off;
-            PlayerPrefs.SetInt(BgmMuteKey, bgmon);
-
-            int sfxon = (isSfxOn) ? On : Off;
-            PlayerPrefs.SetInt(SfxMuteKey, sfxon);
-
-            PlayerPrefs.SetFloat(BgmVolumKey, bgmVolum);
-            PlayerPrefs.SetFloat(SfxVolumKey, sfxVolum);
+            foreach (var state in states.Values)
+            {
+                SaveState(state);
+            }
+        }
+        private void SaveState(State state)
+        {
+            PlayerPrefs.SetInt(state.baseKey + Mute, (state.mute) ? On : Off);
+            PlayerPrefs.SetFloat(state.baseKey + Volum, state.volum);
         }
         #endregion
 
         private void InitializeAudio()
         {
-            bgm.player.mute = !isBgmOn;
+            bgm.player.mute = states[Type.BGM].mute || states[Type.MASTER].mute;
+        }
 
-            foreach (var audio in sfx)
+        public void ChangState(Type type, bool mute, float volum)
+        {
+            State state = states[type];
+
+            //== Updated the list of currently playing sounds
+            if (type == Type.MASTER)
             {
-                audio.player.mute = !isSfxOn;
+                AudioListener.volume = volum;
+            }
+            else if (type == Type.BGM)
+            {
+                UpdateAudio(bgm, in state, in mute, in volum);
+            }
+            else
+            {
+                foreach (var item in sfx)
+                {
+                    if (item.clipID.CompareTo(string.Empty) == 0) continue;
+                    Clip clip = database[item.clipID];
+
+                    if (clip.type != type) continue;
+                    UpdateAudio(item, in state, in mute, in volum);
+                }
+            }
+
+            state.mute = mute;
+            state.volum = volum;
+
+            SaveState(state);
+
+            void UpdateAudio(Audio audio,in State state,in bool mute,in float volum)
+            {
+                if (audio.onProductionUpdate)
+                {
+                    float originalVolum = audio.originalVolum;
+                    if (state.volum != 0) originalVolum /= state.volum;
+                    else originalVolum = 0;
+
+                    audio.originalVolum = originalVolum * volum;
+                }
+                else
+                {
+                    float originalVolum = audio.player.volume;
+                    if (state.volum != 0) originalVolum /= state.volum;
+                    else originalVolum = 0;
+
+                    audio.player.volume = originalVolum * volum;
+                }
+
+                audio.player.mute = mute;
             }
         }
 
-        public void PlayBGMSound(string id, float volum = -1, bool loop = true, Production product = Production.None)
+        //== Return only a clone to prevent direct external modification
+        public State GetStateClone(Type type)
+        {
+            return states[type].Clone();
+        }
+
+        public void PlaySound(string id, float volum = -1, bool loop = true, Production product = Production.None)
         {
             if (!onReady)
             {
@@ -207,47 +313,15 @@ namespace Manager
 
             if (clip == null) { return; }
 
-            if (volum == -1) volum = clip.defaultVolum * bgmVolum;
-            bgm.player.loop = loop;
-            bgm.player.clip = clip.audio;
-
-            if (product != Production.None)
-            {
-                bgm.onProductionUpdate = true;
-                bgm.production = product;
-                bgm.currentTime = 0;
-
-                if (bgm.production == Production.FadeIn)
-                {
-                    bgm.player.volume = 0;
-                }
-                else
-                {
-                    bgm.player.volume = volum;
-                }
-                bgm.originalVolum = volum;
-            }
-            else
-            {
-                bgm.production = Production.None;
-                bgm.onProductionUpdate = false;
-                bgm.player.volume = volum;
-            }
-
-            bgm.player.Play();
+            if (clip.type == Type.BGM) PlayBgm(clip, volum, loop, product);
+            else PlaySfx(clip, volum, loop, product);
         }
-        public void PlaySfxSound(string id, float volum = -1, bool loop = false, Production product = Production.None)
+        private void PlaySfx(Clip clip, float volum = -1, bool loop = false, Production product = Production.None)
         {
-            if (!onReady)
-            {
-                Regist(new Registration(id, volum, false, loop, product));
-                return;
-            }
+            State state = states[clip.type];
 
-            Clip clip = GetClip(id);
-
-            if (clip == null) { return; }
-            if (volum == -1) volum = clip.defaultVolum * sfxVolum;
+            if (volum == -1) volum = clip.defaultVolum * state.volum;
+            else volum *= state.volum;
 
             Audio sound = sfx.Find((item) => item.player.isPlaying == false);
 
@@ -264,33 +338,61 @@ namespace Manager
                 sound = sfx[0];
             }
 
-            sound.player.loop = loop;
-            sound.player.clip = clip.audio;
+            SetAudio(sound, state, clip, volum, loop, product);
+
+            sound.player.Play();
+        }
+        private void PlayBgm(Clip clip, float volum = -1, bool loop = false, Production product = Production.None)
+        {
+            if (volum == -1) volum = clip.defaultVolum * states[Type.BGM].volum;
+            else volum *= states[Type.BGM].volum;
+
+            SetAudio(bgm, states[Type.BGM], clip, volum, loop, product);
+            bgm.player.Play();
+        }
+        private void SetAudio(Audio audio, State state, Clip clip, float volum = -1, bool loop = false, Production product = Production.None)
+        {
+            audio.player.loop = loop;
+            audio.player.clip = clip.audio;
+            audio.player.mute = state.mute || states[Type.MASTER].mute;
+            audio.clipID = clip.id; 
 
             if (product != Production.None)
             {
-                sound.onProductionUpdate = true;
-                sound.production = product;
-                sound.currentTime = 0;
+                audio.onProductionUpdate = true;
+                audio.production = product;
+                audio.currentTime = 0;
 
-                if (sound.production == Production.FadeIn)
+                if (audio.production == Production.FadeIn)
                 {
-                    sound.player.volume = 0;
+                    audio.player.volume = 0;
+                }
+                else if(audio.production == Production.FadeOut)
+                {
+                    audio.player.volume = volum;
                 }
                 else
                 {
-                    sound.player.volume = volum;
+                    Debug.Log("A new sound effect method has been added. " +
+                        "Please implement the corresponding logic for the effect.");
                 }
-                sound.originalVolum = volum;
+                audio.originalVolum = volum;
             }
             else
             {
-                sound.production = Production.None;
-                sound.onProductionUpdate = false;
-                sound.player.volume = volum;
+                audio.production = Production.None;
+                audio.onProductionUpdate = false;
+                audio.player.volume = volum;
             }
+        }
 
-            sound.player.Play();
+        public void PauseAllSound()
+        {
+            AudioListener.pause = true;
+        }
+        public void UnPauseAllSound()
+        {
+            AudioListener.pause = false;
         }
 
         private void Regist(Registration registration)
@@ -301,14 +403,7 @@ namespace Manager
         {
             Registration data = callStack.Dequeue();
 
-            if (data.isBgm)
-            {
-                PlayBGMSound(data.id, data.defaultVolum, data.isLoop, data.fade);
-            }
-            else
-            {
-                PlaySfxSound(data.id, data.defaultVolum, data.isLoop, data.fade);
-            }
+            PlaySound(data.id, data.defaultVolum, data.isLoop, data.fade);
         }
         private Clip GetClip(string id)
         {
@@ -328,9 +423,9 @@ namespace Manager
 
             return database[id];
         }
-        private void AudioFade(Audio audio)
+        private void AudioProduct(Audio audio)
         {
-            if(audio.production == Production.None)
+            if (audio.production == Production.None)
             {
                 audio.onProductionUpdate = false;
                 return;
@@ -343,7 +438,7 @@ namespace Manager
             #region Production
             if (audio.production == Production.FadeIn)
             {
-                if(1 <= ratio)
+                if (1 <= ratio)
                 {
                     audio.onProductionUpdate = false;
                     audio.player.volume = originalVolum;
@@ -353,7 +448,7 @@ namespace Manager
                     audio.player.volume = originalVolum * ratio;
                 }
             }
-            else if(audio.production == Production.FadeOut)
+            else if (audio.production == Production.FadeOut)
             {
                 if (1 <= ratio)
                 {
@@ -378,12 +473,12 @@ namespace Manager
             if (onReady)
             {
                 //== Handle production
-                if (bgm.onProductionUpdate) { AudioFade(bgm); }
+                if (bgm.onProductionUpdate) { AudioProduct(bgm); }
                 foreach (var item in sfx)
                 {
                     if (item.onProductionUpdate)
                     {
-                        AudioFade(item);
+                        AudioProduct(item);
                     }
                 }
 
