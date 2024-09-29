09bbabbdb18a50e364087cfa8b2dedfb738b6774
LeeHyeonCheol <60866286+Cocoa303@users.noreply.github.com>
2024-09-30 00:36:43 +0900

    Add : Sound Button, Sound Button Editor
    - 버튼에 음향을 출력하는 기능이 포함된 클래스를 추가하였습니다.
    - Sound Manager와 연동되어 있기 때문에 Sound Manager에 데이터가 없으면 해당 클래스는 작동하지 않습니다.
    - Clips의 Key값을 직접 입력하는 것이 아닌, Custom Editor를 통해 Popup 형태로 선택하는 방식으로 구현되어 있습니다.
    - 이는 프로그래머가 아닌, 다른 파트의 팀원이 편히 음향 작업을 진행할 수 있도록 하기 위하여 구상한 방법입니다.

---
+++ Library/Assets/Editor/SoundButtonEditor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(Common.SoundButton))]
+public class SoundButtonEditor : Editor
+{
+    protected override void OnHeaderGUI()
+    {
+        base.OnHeaderGUI();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //== Sound Manager를 탐색하여 가지고있는 데이터들을 출력하기
+        var names = Manager.Sound.Instance.EditorClipNames;
+        var data = (Common.SoundButton)target;
+
+        EditorGUILayout.LabelField("Select Sound Clip");
+        //== 탐색
+        int findIndex = names.FindIndex(name => name == data.SoundKey);
+        if (findIndex != -1)
+        {
+            int select = EditorGUILayout.Popup("Select Option", findIndex, names.ToArray());
+            data.SoundKey = names[select];
+        }
+        else
+        {
+            names.Insert(0, "Select Sound Name");
+            int select = EditorGUILayout.Popup("Select Option", 0, names.ToArray());
+            data.SoundKey = names[select];
+        }
+
+        base.OnInspectorGUI();
+    }
+}
+
---
+++ Library/Assets/Scripts/00.Common/SoundButton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common
+{
+    public class SoundButton : MonoBehaviour
+    {
+        [SerializeField] Button trigger;
+        [SerializeField, ReadOnly] string soundKey;
+        [SerializeField] float volume = -1;
+        [SerializeField] bool isLoop;
+
+        public string SoundKey { get => soundKey; set => soundKey = value; }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (trigger == null)
+            {
+                trigger = GetComponent<Button>();
+            }
+        }
+
+#endif
+        private void Start()
+        {
+            trigger.onClick.AddListener(OnClick);
+        }
+
+        private void OnClick()
+        {
+            Manager.Sound.Instance.PlaySound(soundKey, volume, isLoop);
+        }
+    }
+
+}
